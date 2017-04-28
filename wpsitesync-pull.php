0b2f787bda4b5a98e846ebbcc2a2d5146f87c4d6
Dave Jesch <dave@davejesch.com>
2017-04-28 09:03:06 -0700

    update version number

--- wpsitesync-pull.php
+++ wpsitesync-pull.php
@@ -5,7 +5,7 @@ Plugin URI: http://wpsitesync.com
 Description: Allow Content Creators to "Pull" Content from the Target site into the Source site.
 Author: WPSiteSync
 Author URI: http://wpsitesync.com
-Version: 1.2
+Version: 2.0
 Text Domain: wpsitesync-pull
 
 The PHP code portions are distributed under the GPL license. If not otherwise stated, all
@@ -28,7 +28,7 @@ if (!class_exists('WPSiteSync_Pull')) {
 		private static $_instance = NULL;
 
 		const PLUGIN_NAME = 'WPSiteSync for Pull';
-		const PLUGIN_VERSION = '1.2';
+		const PLUGIN_VERSION = '2.0';
 		const PLUGIN_KEY = '4151f50e546c7b0a53994d4c27f4cf31'; // '1a127f14595c88504b22839abc40708c';
 
 		private $_license = NULL;
